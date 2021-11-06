7194cd5b2f012abfea7cfcb89bb6fdaab62b7c8c
nelson <zetalatakamnelson@yahoo.fr>
2021-11-06 13:07:30 +0100

    initial commit

---
+++ backend-java-noteservice/src/main/java/com/zetalatakam/rest/noteservice/NoteserviceApplication.java
@@ -0,0 +1,13 @@
+package com.zetalatakam.rest.noteservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteserviceApplication.class, args);
+	}
+
+}
